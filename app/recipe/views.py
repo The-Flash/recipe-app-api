abe8a1c7fa3e06ae268373df1f2e79729080ffd4
FLASH <flash@flash>
2019-03-05 22:42:41 +0000

    Added custome recipe detail view

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -43,4 +43,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         """Retrieve the recipe for authenticated user"""
         return self.queryset.filter(user=self.request.user)
+    
+    def get_serializer_class(self):
+        """Return appropriate serializer class"""
+        if self.action == "retrieve":
+            return serializers.RecipeDetailSerializer
+        return self.serializer_class
 
